846a1fca824f1fa2a39cdf475cec39d56ae0b469
abraz73 <80893442+abraz73@users.noreply.github.com>
2024-08-17 02:12:31 +0530

    Update info.py

--- info.py
+++ info.py
@@ -31,7 +31,7 @@ auth_grp = environ.get('AUTH_GROUP')
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://abraz123:aqIz8BbFPQY0cYuw@lamautofilter.3ad1o.mongodb.net/?retryWrites=true&w=majority&appName=LamAutoFilter")
+DATABASE_URI = environ.get('DATABASE_URI', "mongodb+srv://abraz123:<aqIz8BbFPQY0cYuw>@lamautofilter.3ad1o.mongodb.net/?retryWrites=true&w=majority&appName=LamAutoFilter")
 DATABASE_NAME = environ.get('DATABASE_NAME', "Cluster0")
 COLLECTION_NAME = environ.get('COLLECTION_NAME', 'Telegram_files')
 
